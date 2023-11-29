f7a8db50cc110670f832d93f6c3bdb938ae16685
DialecticalLaw <d.shmuratkin@mail.ru>
2023-11-29 20:41:07 +0300

    feat: add the ability to open a burger menu

---
+++ coffee-house/index.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const burgerMenuContent = document.querySelector('.burger-menu-content');
+const burgerButton = document.querySelector('.burger-button');
+
+burgerButton.addEventListener('click', toggleBurger);
+
+function toggleBurger() {
+    window.scrollTo(0, 0);
+    body.classList.toggle('lock');
+    burgerMenuContent.classList.toggle('burger-menu-active');
+    burgerButton.classList.toggle('burger-button-active');
+}
+
+document.addEventListener('click', closeInterface);
+
+function closeInterface(event) {
+    if (burgerButton.classList.contains('burger-button-active')) {
+        if (event.target.classList.contains('link') && !event.target.classList.contains('active-menu-link-header')) {
+            body.classList.remove('lock');
+            burgerMenuContent.classList.remove('burger-menu-active');
+            burgerButton.classList.remove('burger-button-active');
+        }
+    }
+}
