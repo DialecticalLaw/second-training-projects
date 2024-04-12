16150a7d8781fde0567243edeeacb6792d32a792
DialecticalLaw <d.shmuratkin@mail.ru>
2024-04-12 22:38:16 +0300

    feat: change design

--- fun-chat/src/app/view/components/footer-view/footer-view.ts
+++
@@ -1,7 +0,0 @@
-import { container } from '../container/container';
-import { footer } from '../main-page/footer/footer';
-
-export function drawFooter() {
-  container.append(footer);
-  footer.hidden = false;
-}
---
+++ fun-chat/src/app/view/footer-view/footer-view.ts
@@ -0,0 +1,7 @@
+import { container } from '../components/container/container';
+import { footer } from '../components/main-page/footer/footer';
+
+export function drawFooter() {
+  container.append(footer);
+  footer.hidden = false;
+}
