afb3d6ad61f3dad524e0526033863308b6df5d2d
DialecticalLaw <d.shmuratkin@mail.ru>
2024-04-15 13:52:35 +0300

    fix: fix user search function

--- fun-chat/src/app/utils/is-match.ts
+++ fun-chat/src/app/utils/is-match.ts
@@ -1,6 +1,7 @@
 export function isMatch(text: string, template: string): boolean {
   for (let i: number = 0; i < template.length; i += 1) {
-    if (text[i] !== template[i]) {
+    if (text[i] === undefined) return false;
+    if (text[i].toUpperCase() !== template[i].toUpperCase()) {
       return false;
     }
   }
